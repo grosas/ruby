8114a2d68723cf62143856d142de9758dea4ff72
Gabe Rosas <ge.rosas@me.com>
2013-10-13 00:53:32 -0500

    ruby training - initial commit

---
+++ lrthw/ex3.rb
@@ -0,0 +1,41 @@
+# prints string
+puts "I will now count my chickens:"
+
+#prints string, performs math operation
+puts "Hens", 25 + 30 / 6
+
+#prints string, performs math operation
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+#print string
+puts "Now I will count the eggs:"
+
+#performs math operation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+#prints string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#compares both sides of the "less than" returns boolean
+puts 3 + 2 < 5 - 7
+
+#prints string, math operation
+puts "What is 3 + 2?", 3 + 2.00
+
+#prints string, math operation
+puts "What is 5 - 7?", 5.0 - 7
+
+#prints string
+puts "Oh, that's why it's false."
+
+#prints string
+puts "How about some more."
+
+#prints string, comparison with boolean result
+puts "Is it greater?", 5 > -2
+
+#prints string, comparison with boolean result
+puts "Is it greater or equal?", 5 >= -2
+
+#prints string, comparison with boolean result
+puts "Is it less or equal?", 5 <= -2
