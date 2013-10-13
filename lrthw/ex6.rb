103733af6f7de705d9d96e6ec92ec58b00ce5c7b
Gabe Rosas <ge.rosas@me.com>
2013-10-13 03:31:03 -0500

    added ex6 & ex7

---
+++ lrthw/ex6.rb
@@ -0,0 +1,42 @@
+# a string with interpolation of a hard coded value?
+x = "There are #{10} types of people."
+
+# the binary variable equals a string that says binary
+binary = "binary"
+
+# variable do_not equals the string "don't"
+do_not = "don't"
+
+# y is assigned a string with string interpolation
+y = "Those who know #{binary} and those who #{do_not}."
+
+# print X 
+puts x
+
+# print Y
+puts y
+
+# print a string with another string injected via string interpolation
+puts "I said:  #{x}."
+
+# print a string with another string injected via string interpolation
+puts "I also said: '#{y}'."
+
+# the variable is assigned boolean response false.
+hilarious = false
+
+# the variable is assigned a string with a boolean variable string interpolation.
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# this prints the variable that is assigned a boolean variable via string interpolation.
+puts joke_evaluation
+
+# a string
+w = "This is the left side of ... "
+
+#another string
+e = "a string with a right side."
+
+# the strings are printed and concatenated.
+puts w + e
+
