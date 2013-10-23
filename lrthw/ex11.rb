fd5a7da9246775ce0be636a14b3e492b7f3ab4d8
Gabe Rosas <ge.rosas@me.com>
2013-10-23 04:36:04 -0500

    Adding today's exercises

---
+++ lrthw/ex11.rb
@@ -0,0 +1,10 @@
+print "How old are you? "
+
+# the gets method reads user input.  the chomp method 
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
