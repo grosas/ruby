09737b580175644db7070d4ba12ca8f96998c989
Gabe Rosas <ge.rosas@me.com>
2013-10-23 07:37:10 -0500

    completed today's work

--- codeacademy/evernote api/open placekitten.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-
-# Open http://placekitten.com/ for reading on line 4!
-kittens = open('http://placekitten.com')
-body = kittens.read[559, 441]
-
-# Add your puts statement below!
-puts body
